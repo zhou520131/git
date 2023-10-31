9e40d186799fbbf21e8b0563f46aa7a9fd65a8a3
zhou <15537615287@163.com>
2023-10-31 14:45:26 +0800

    sixth commit

--- src/main/java/com/li/git/git.java
+++ src/main/java/com/li/git/git.java
@@ -5,6 +5,6 @@ public class git {
         System.out.println("hello");
         System.out.println("world");
         System.out.println("ho ho");
-
+        System.out.println("master test");
     }
 }
