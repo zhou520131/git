08c4e0c42bd62231abf6d6a720dfa9ea1e7cb4da
zhou <15537615287@163.com>
2023-10-31 14:17:40 +0800

    third commit

--- src/main/java/com/li/git/git.java
+++ src/main/java/com/li/git/git.java
@@ -4,6 +4,7 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+        System.out.println("ho ho");
 
     }
 }
